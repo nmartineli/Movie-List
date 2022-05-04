ebf8858740c77a255e8cadc45912c1d70dcaf38c
unknown <nataliamartineli@gmail.com>
2022-05-04 11:58:28 -0300

    Remove from List functionality

--- src/components/ListButton/index.js
+++ src/components/ListButton/index.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToList } from '../../redux/ducks/movieList';
+import { addToList, removeFromList } from '../../redux/ducks/movieList';
 
 export default function ListButton(props) {
+	const [onList, setOnList] = useState(false);
+
 	const { movie } = props;
 
 	const movies = useSelector((state) => state.movies.movies);
@@ -18,6 +21,23 @@ export default function ListButton(props) {
 		const addedMovie = findMovie(event.target.value);
 		console.log(addedMovie);
 		dispatch(addToList(addedMovie));
+		setOnList(true);
+	}
+
+	function handleRemoveFromList(event) {
+		event.preventDefault();
+		const removedMovie = findMovie(event.target.value);
+		console.log(removedMovie);
+		dispatch(removeFromList(removedMovie));
+		setOnList(false);
+	}
+
+	if (onList) {
+		return (
+			<Button variant="secondary" value={movie.id} onClick={handleRemoveFromList}>
+				Remover da lista
+			</Button>
+		);
 	}
 
 	return (
--- src/redux/ducks/movieList.js
+++ src/redux/ducks/movieList.js
@@ -1,9 +1,17 @@
 const ADD_TO_LIST = 'ADD_TO_LIST';
+const REMOVE_FROM_LIST = 'REMOVE_FROM_LIST';
 
 export function addToList(movie) {
 	return {
 		type: 'ADD_TO_LIST',
-		movie,
+		addedMovie: movie,
+	};
+}
+
+export function removeFromList(movie) {
+	return {
+		type: 'REMOVE_FROM_LIST',
+		removedMovie: movie,
 	};
 }
 
@@ -14,9 +22,13 @@ const initialState = {
 export default (state = initialState, action) => {
 	switch (action.type) {
 		case ADD_TO_LIST:
-			const { movie } = action;
-			const newList = [...state.movieList, movie];
+			const { addedMovie } = action;
+			const newList = [...state.movieList, addedMovie];
 			return { ...state, movieList: newList };
+		case REMOVE_FROM_LIST:
+			const { removedMovie } = action;
+			const updatedList = state.movieList.filter((m) => m.id !== removedMovie.id);
+			return { ...state, movieList: updatedList };
 		default:
 			return state;
 	}
