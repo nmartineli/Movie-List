bdd39ad863126487de4dffe1d9c2b0c2ace71cf0
unknown <nataliamartineli@gmail.com>
2022-05-04 20:02:20 -0300

    Button conditional render for movies on watchlist

--- src/components/ListButton/index.js
+++ src/components/ListButton/index.js
@@ -1,15 +1,11 @@
-import { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToList, removeFromList } from '../../redux/ducks/movieList';
 
 export default function ListButton(props) {
-	const [onList, setOnList] = useState(false);
-
 	const { movie } = props;
-
 	const movies = useSelector((state) => state.movies.movies);
-
+	const movieList = useSelector((state) => state.movieList.movieList);
 	const dispatch = useDispatch();
 
 	function findMovie(id) {
@@ -19,30 +15,26 @@ export default function ListButton(props) {
 	function handleAddToList(event) {
 		event.preventDefault();
 		const addedMovie = findMovie(event.target.value);
-		console.log(addedMovie);
 		dispatch(addToList(addedMovie));
-		setOnList(true);
 	}
 
 	function handleRemoveFromList(event) {
 		event.preventDefault();
 		const removedMovie = findMovie(event.target.value);
-		console.log(removedMovie);
 		dispatch(removeFromList(removedMovie));
-		setOnList(false);
 	}
 
-	if (onList) {
+	if (movieList.find((m) => m.id === movie.id) !== undefined) {
 		return (
 			<Button variant="secondary" value={movie.id} onClick={handleRemoveFromList}>
 				Remover da lista
 			</Button>
 		);
+	} else {
+		return (
+			<Button variant="secondary" value={movie.id} onClick={handleAddToList}>
+				Adicionar à lista
+			</Button>
+		);
 	}
-
-	return (
-		<Button variant="secondary" value={movie.id} onClick={handleAddToList}>
-			Adicionar à lista
-		</Button>
-	);
 }
